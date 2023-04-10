f2e5dc340bf9533578f824ed16afd622daa572a6
Bolbotunov <100226065+Bolbotunov@users.noreply.github.com>
2023-04-10 22:03:43 +0300

    feat: add select

--- js.js
+++ js.js
@@ -1,3 +1,14 @@
+let roadsList = {
+    m1:200,
+    m2:300,
+    m3:400,
+    m4:330,
+    m5:654,
+    m6:103,
+}
+
+let roads = document.getElementById('roads')
+// let roads= document.getElementById('list')
 let km = document.getElementById('dist')
 let weight = document.getElementById('weight')
 let kmBtn = document.querySelector('.count')
@@ -5,11 +16,29 @@ let fines = document.getElementById('fines')
 let resetBtn = document.querySelector('.reset')
 let loading = document.querySelector('.loading')
 let getFine = document.querySelector('.getFine')
+
 function count(){
     loading.innerHTML='Completed'
     return fines.value = km.value * weight.value,
     getFine.innerHTML=`You will get fine: ${fines.value}`
 }
+
+let kmOfRoads = Object.values(roadsList)
+
+roads.onclick = () => {
+    for(let i=0; i<kmOfRoads.length;i++){
+        if(roads.value === i.toString()){
+            console.log(kmOfRoads[i])
+            km.value = kmOfRoads[i]
+        }
+    }
+}
+
+roads.addEventListener("change", function(e) {
+    console.log(e.target.value);
+  });
+
+
 
 
 kmBtn.onclick = () => {
@@ -17,8 +46,6 @@ kmBtn.onclick = () => {
     loading.innerHTML='loading...'
     
 }
-
-
 
 function reset(){
     fines.value = ''
@@ -32,3 +59,4 @@ function reset(){
     resetBtn.onclick = () => {
         reset()
     }
+
